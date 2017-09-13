3a84d3907e090c9d95d70958bbc6252690c62352
Vladislav <vlad.aleev@gmail.com>
2017-09-13 22:22:54 +0200

    feat:add some changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,9 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import autosize from 'autosize';
 
 $(() => {
 	svg4everybody();
 });
+
+autosize(document.querySelector('textarea'));
