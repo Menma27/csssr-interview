81a7069729c09f6efe4ed9674448970d059ee71d
Vladislav <vlad.aleev@gmail.com>
2017-09-21 13:26:08 +0200

    feat(finish v5)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,6 @@ import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 import autosize from 'autosize';
 import '../blocks/javascript-level/javascript-level';
-// import '../blocks/about-words/about-words';
 
 $(() => {
 	svg4everybody();
