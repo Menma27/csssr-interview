2fef6ce30ef1fb1ce3427ce0caa03b0e3731387e
Vladislav <vlad.aleev@gmail.com>
2017-09-21 13:19:41 +0200

    feat(finish v4)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,9 +2,13 @@ import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 import autosize from 'autosize';
 import '../blocks/javascript-level/javascript-level';
+// import '../blocks/about-words/about-words';
 
 $(() => {
 	svg4everybody();
 });
 
-autosize(document.querySelector('textarea'));
+setTimeout(() => { // Небольшая задержка для подзагрузки
+	document.getElementById('textarea').value = document.getElementById('textarea').value.replace(/\n/g, ' ');
+	autosize(document.getElementById('textarea'));
+}, 1);
